bdf467ea0655aeb6f49c31d0e268849fee9d6f34
AlexZ33 <775136985@qq.com>
2017-07-26 22:05:26 +0800

    first commit

---
+++ 09-TreeMap/2-vue-d3-tree/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    
+    <Tree></Tree>
+    <Bundle></Bundle>
+  </div>
+</template>
+
+<script>
+import Tree from './components/tree'
+
+import Bundle from './components/HierarchicalEdgeBundling'
+
+export default {
+  name: 'app',
+  components: {
+    Tree,
+    Bundle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 09-TreeMap/2-vue-d3-tree/src/components/tree.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="viewport treeclass" v-resize = "resize">
+		
+	</div>
+</template>
+
+<script>
+	import resize from "vue-resize-directive"
+	import euclidean from "./euclidean-layout"
+	import circular from "./circular-layout"
+
+
+	import {compareString , anchorTodx, drawLink, toPromise, findInParents, mapMany, removeTextAndGraph, translate} from './d3-utils'
+
+
+	import * as d3 from "d3"
+	import * as d3Hierarchy from "d3-hierarchy"
+
+
+	export default{
+
+		name: "tree",
+		props: {
+			
+		}
+		data(){
+          return {
+
+          };
+		},
+		mounted () {
+			this.render();   //
+		}
+	}
+</script>
+
+<style>
+	
+</style>
